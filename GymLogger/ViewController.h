c693be705f29f9b4488579a31dbf24bce2e60def
Dan Sinclair <dansinclair@me.com>
2015-07-31 18:24:53 +0100

    Initial commit

---
+++ GymLogger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GymLogger
+//
+//  Created by Dan Sinclair on 31/07/2015.
+//  Copyright (c) 2015 Dan Sinclair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
